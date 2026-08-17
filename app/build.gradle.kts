c874c729683c3360a69cd73fed66ba0ae82d54e7
seyelatyr <122128820+seyelatyr@users.noreply.github.com>
2026-08-17 20:17:43 +0000

    Bump SUD FM to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.newandromo.dev1660662.app2146388"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1046
-        versionName = "2.0.0"
+        versionCode = 1047
+        versionName = "2.0.1"
     }
 
     buildTypes {
